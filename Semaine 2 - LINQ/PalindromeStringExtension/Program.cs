5fd46181b1cfd0d60f877c1141affd365d374b6a
olivier.choquet@vinci.be <olivier.choquet@vinci.be>
2024-09-29 11:10:13 +0200

    Semaine 2

---
+++ Semaine 2 - LINQ/PalindromeStringExtension/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using StringExtensions;
+
+Console.WriteLine("test IsAPalindrome");
+
+Console.WriteLine("été est un palindrome ? " + "été".IsAPalindrome());
+
+Console.WriteLine("bonjour est un palindrome ? " + "bonjour".IsAPalindrome());
---
+++ Semaine 2 - LINQ/PalindromeStringExtension/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExtensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsAPalindrome(this string s)
+        {
+            if (s == null) return false;
+            if (s.Length == 0) return false;
+
+            string reverse = new string(s.Reverse().ToArray());
+
+            if (reverse == s) return true;
+            else return false;
+            
+        }
+    }
+}
