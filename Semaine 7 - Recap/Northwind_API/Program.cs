f33bbaf3f5beb20e18003cc422cdcbf46165aefd
olivier.choquet@vinci.be <olivier.choquet@vinci.be>
2024-11-08 09:38:29 +0100

    Semaine 7

---
+++ Semaine 7 - Recap/Northwind_API/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.HttpLogging;
+using Northwind_API;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddHttpLogging(options =>
+{
+    options.LoggingFields = HttpLoggingFields.All;
+    options.RequestBodyLogLimit = 4096;
+    options.ResponseBodyLogLimit = 4096;
+});
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen( c =>
+    {
+        c.SwaggerDoc("v1", new() { Title = "Northwind API", Version = "v1", Description = "Northwind API By Olivier Choquet" });
+        c.SchemaFilter<SwaggerSchemaFilter>();
+ 
+    });
+
+var app = builder.Build();
+
+app.UseHttpLogging();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+
+
---
+++ Semaine 7 - Recap/Northwind_API/SwaggerSchemaFilter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Northwind_API
+{
+    public class SwaggerSchemaFilter : ISchemaFilter
+    {
+
+        public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+        {
+
+            foreach (var item in context.SchemaRepository.Schemas.Keys)
+            {
+                if (!item.EndsWith("DTO")) context.SchemaRepository.Schemas.Remove(item);
+            }
+
+            context.SchemaRepository.Schemas.Remove("Void");
+
+        }
+    }
+}
