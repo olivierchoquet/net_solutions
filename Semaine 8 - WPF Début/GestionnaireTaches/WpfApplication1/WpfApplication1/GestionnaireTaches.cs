7643c0bfb093166472edb0963017ca1ef374be7d
olivier.choquet@vinci.be <olivier.choquet@vinci.be>
2024-11-15 11:55:32 +0100

    Semaine 8

---
+++ Semaine 8 - WPF Début/GestionnaireTaches/WpfApplication1/WpfApplication1/GestionnaireTaches.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    public class GestionnaireTaches
+    {
+        private IList<Tache> taches;
+
+        public GestionnaireTaches()
+        {
+            taches = new List<Tache>();
+            taches.Add(new Tache("super tâche"));
+        }
+
+        public IList<Tache> ListeTaches {
+            get { return taches; }
+        }
+    }
+
+    public class Tache
+    {
+        private string _description;
+        private int _priorité;
+        private DateTime _date;
+        private bool _termine;
+
+        public Tache(string description)
+        {
+            _description = description;
+            _priorité = 3;
+            _date = DateTime.Now;
+            _termine = false;
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        public int Priorité
+        {
+            get { return _priorité; }
+            set { _priorité = value; }
+        }
+
+        public DateTime Date
+        {
+            get { return _date.Date; }
+            set { _date = value; }
+        }
+
+        public bool Termine
+        {
+            get { return _termine; }
+            set { _termine = value; }
+        }
+    }
+}
---
+++ Semaine 8 - WPF Début/GestionnaireTaches/WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        GestionnaireTaches gest = new GestionnaireTaches();
+       
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            datagrid.DataContext = gest.ListeTaches;
+            IList<int> priorités = new List<int>();
+            for (int i=1;i<=5;i++)
+            {
+                priorités.Add(i);
+            }
+            PrioriteValeur.DataContext = priorités;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            datagrid.SelectedItem = null;
+            DescriptionValeur.Text = "";
+            PrioriteValeur.SelectedItem = 3;
+           
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            
+                Tache nouvelleTache = new Tache(DescriptionValeur.Text);
+                nouvelleTache.Priorité = (int)PrioriteValeur.SelectedItem;
+                nouvelleTache.Date = (DateTime) DateValeur.SelectedDate;
+                nouvelleTache.Termine = TermineValeur.IsChecked.Value;
+                gest.ListeTaches.Add(nouvelleTache);
+                datagrid.Items.Refresh(); 
+                
+            
+    
+            
+            
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Tache modifTache = (Tache)datagrid.SelectedItem;
+            gest.ListeTaches.Remove(modifTache);
+            datagrid.Items.Refresh(); 
+
+           
+        }
+    }
+}
