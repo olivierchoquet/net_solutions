09a81b095765bb98c08691f9abc712f8517b5220
olivier.choquet@vinci.be <olivier.choquet@vinci.be>
2024-11-22 12:23:38 +0100

    Semaine 9

---
+++ Semaine 9 - WPF MVVM 1/WpfEmployee/ViewModels/EmployeeVM.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfEmployee.Models;
+
+namespace WpfApplication1.ViewModels
+{
+    public class EmployeeVM 
+    {
+
+        #region fields
+
+        private NorthwindContext dc = new NorthwindContext();
+        private List<EmployeeModel> _EmployeesList;
+        private List<string> _listTitle;
+
+        #endregion
+
+        
+
+        public List<EmployeeModel> EmployeesList
+        {
+            get {
+                return _EmployeesList = _EmployeesList ?? loadEmployee(); 
+               
+            }
+           
+        }
+  
+        private List<EmployeeModel> loadEmployee()
+        {
+            List<EmployeeModel> localCollection = new List<EmployeeModel>();
+            foreach (var item in dc.Employees)
+            {
+                localCollection.Add(new EmployeeModel(item));
+                
+            }
+            
+            return localCollection;
+            
+        }
+
+
+        public List<string> ListTitle
+        {
+            get { return _listTitle = _listTitle ?? LoadTitleOfCourtesy(); }
+
+        }
+
+        private List<string> LoadTitleOfCourtesy()
+        {
+            return dc.Employees.Select(e => e.TitleOfCourtesy).Distinct().ToList();
+        }
+
+       
+
+       
+    }
+}
